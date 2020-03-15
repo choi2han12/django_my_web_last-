18a82de007c4ae86f7f68975f77b08d8b7a1fc8e
choiYeonJoon <tvxq032@daum.net>
2020-03-15 17:06:30 +0900

    blog post list

--- blog/models.py
+++ blog/models.py
@@ -10,4 +10,4 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=True)
 
     def __str__(self):
-        return '{}::{}'.format(self.title , self.author)
+        return '{}::{}'.format(self.title,self.author)
