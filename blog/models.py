737a18db1ad5ad3cab3ad2941081ac08b0fbf4eb
choiYeonJoon <tvxq032@daum.net>
2020-03-16 20:36:50 +0900

    show

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
-
+from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=80)
     content = models.TextField()
-
+    head_image = models.ImageField(upload_to='blog/%Y/%m/%d', blank=True)
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
 
     def __str__(self):
-        return '{}::{}'.format(self.title,self.author)
+        return '{}::{}'.format(self.title, self.author)
