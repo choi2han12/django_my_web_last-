863f44ab188f7cb25829d87a4032ff1896e833f7
choiYeonJoon <tvxq032@daum.net>
2020-03-27 02:25:04 +0900

    category in post list

--- blog/tests.py
+++ blog/tests.py
@@ -1,17 +1,53 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.utils import timezone
-from .models import Post
+from .models import Post, Category
 from django.contrib.auth.models import User
 
-def create_post(title,content,author):
-    blog_post =Post.objects.create(
+def create_category(name='life', description=''):
+    category, is_created = Category.objects.get_or_create(
+        name=name,
+        description=description
+    )
+    return category
+
+
+def create_post(title, content, author, category=None):
+    blog_post = Post.objects.create(
         title=title,
         content=content,
         created=timezone.now(),
-        author=author
+        author=author,
+        category=category,
     )
     return blog_post
+
+
+class TestModel(TestCase):
+    def setUp(self):
+        self.client=Client()
+        self.author_000=User.objects.create(username='smith', password='nopassword')
+
+    def test_category(self):
+        category = create_category()
+        post_000 = create_post(
+            title='The first',
+            content='Hello',
+            author=self.author_000,
+            category=category,
+        )
+        self.assertEqual(category.post_set.count(),1)
+
+
+    def test_post(self):
+        category =create_category()
+        post_000 = create_post(
+            title='The first',
+            content='Hello',
+            author=self.author_000,
+            category=category,
+        )
+
 
 
 class Testview(TestCase):
@@ -58,7 +94,13 @@ class Testview(TestCase):
         soup=BeautifulSoup(response.content,'html.parser')
         title=soup.title
         body=soup.body
+        post_000_read_more_btn = body.find('a', id='read-more-post-{}'.format(post_000.pk))
+        # 왜 값이 안찾아지는지??
+
+        print(post_000_read_more_btn)
         main_div=body.find('div', id='main_div')
         self.assertIn(post_000.title, main_div.text)
         self.assertIn(post_000.author.username, main_div.text)
-
+        # self.assertIn(post_000.author.username, main_div.text)
+        category_card=body.find('div',id='category-card')
+        self.assertIn('미분류',category_card.txt)
